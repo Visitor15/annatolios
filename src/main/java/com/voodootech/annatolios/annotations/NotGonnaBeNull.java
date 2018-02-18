28264235c1b6f58de668ebc68c29c624d27715c9
Nick C <visitor15@gmail.com>
2018-02-17 20:02:32 -0500

    Removed annotations. Fixed test directory. Updated pom.

--- src/main/java/com/voodootech/annatolios/annotations/NotGonnaBeNull.java
+++
@@ -1,7 +0,0 @@
-package com.voodootech.annatolios.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.CLASS)
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
-public @interface NotGonnaBeNull { }
